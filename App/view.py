826a3ca512a1bfc261b0d0313f65d40366d30a99
n.mercado <n.mercado@uniandes.edu.co>
2021-05-06 19:14:35 -0500

    cambios

--- App/view.py
+++ App/view.py
@@ -92,8 +92,9 @@ while True:
         valor_maxEnergy = input('Inserte el valor máximo de Energy: ')
         valor_minDanceability = input('Inserte el valor mínimo de Danceability: ')
         valor_maxDanceability = input('Inserte el valor máximo de Danceability: ')
-        respuesta = controller.requerimiento2(valor_minEnergy, valor_maxEnergy, valor_minDanceability, valor_maxDanceability, catalog)
         print("\nCargando información de los videos ....")
+        respuesta = controller.requerimiento2(valor_minEnergy, valor_maxEnergy, valor_minDanceability, valor_maxDanceability, catalog)
+        print(respuesta)
     elif int(inputs[0]) == 4:
         print("\nCargando información de los videos ....")
     elif int(inputs[0]) == 5:
